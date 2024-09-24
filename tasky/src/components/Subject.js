3f1640bc784c3eddc851ab3b6ea4c8390173290a
SytxNox <seven_night_w@hotmail.com>
2024-09-24 13:56:40 +0100

    Completed Tasky Lab 1

---
+++ tasky/src/components/Subject.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Subject = (props) =>{
+    return( 
+        <p>This is a subject!sdhkajhdjkashdkjah{props.title}</p>
+        
+    )
+
+
+}
+export default Subject;
