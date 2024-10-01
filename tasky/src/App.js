7e9ebc315e462eb6ba39fff0b0ca3c9e60697f45
SytxNox <seven_night_w@hotmail.com>
2024-10-01 17:24:28 +0100

    Exercise one

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -8,9 +8,9 @@ function App() {
 
   const [ taskState, setTaskState ] = useState({
     tasks: [
-      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today", done: false },
-      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", done: false },
-      { id: 3, title: "Tidy up", deadline: "Today", done: false}
+      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today", done: false, priority:"Low" },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", done: false,priority:"Medium" },
+      { id: 3, title: "Tidy up", deadline: "Today", done: false, priority:"High"}
     ]
   });
 
@@ -72,6 +72,7 @@ function App() {
           title={task.title}
           description={task.description}
           deadline={task.deadline}
+          priority={task.priority}
           key={task.id}
           done={task.done}
           markDone={() => doneHandler(index)}
